2f6dac6c477de6d4cfa4c67bbaa316e4864b4868
karan <karankumarshk7@gmail.com>
2026-06-09 18:12:36 +0530

    Add cookie-parser and cors middleware, implement error handling, and create utility classes for API responses and errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import "dotenv/config"
 import connectDB from "./db/index.js";
 
-connectDB();
+connectDB()
+.then(()=>{
+    const server=app.listen(process.env.PORT||8000, () => {
+        console.log(`Server is running on port ${process.env.PORT||8000}`);
+    });
+
+    // FOR NETWORK RELATED ISSUE ON SERVER
+    server.on("error", (error) => {
+        console.error("Server error: ", error);
+        process.exit(1); 
+    });
+})
+.catch(error => {
+    console.error("MONGO db connection failed !!!", error);
+    process.exit(1); 
+});
---
+++ src/utils/ApiError.js
@@ -0,0 +1,15 @@
+class ApiError extends Error{
+    constructor(statusCode,message="Somethihng went wrong",errors=[]){
+        super(message)
+        this.statusCode=statusCode
+        this.errors=errors
+        this.data=null
+        // STACK TRACE FOR DEBUGGING PURPOSE
+        // ONLY IN DEVELOPMENT ENVIRONMENT
+        // STACK AND MESSAGE IS TAKE CARE BY DEFAULT IN ERROR OBJECT
+        this.success=false
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+export { ApiError }
