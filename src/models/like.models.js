1ec764c48f56a596e8754b4e93a140b139985d55
karan <karankumarshk7@gmail.com>
2026-06-19 04:07:52 +0530

    Add models for Comment, Like, Playlist, and Tweet with appropriate schemas

---
+++ src/models/like.models.js
@@ -0,0 +1,26 @@
+﻿import mongoose, { Schema } from 'mongoose';
+
+const likeSchema = new Schema({
+  video: {
+    type: Schema.Types.ObjectId,
+    ref: 'Video',
+    required: true,
+  },
+  tweet: {
+    type: Schema.Types.ObjectId,
+    ref: 'Tweet',
+  },
+  comment: {
+    type: Schema.Types.ObjectId,
+    ref: 'Comment',
+  },
+  likedBy:{
+    type:Schema.Types.ObjectId,
+    ref:"User"
+  }
+}, { timestamps: true });
+
+// likeSchema.index({ user: 1, tweet: 1 }, { unique: true, sparse: true });
+// likeSchema.index({ user: 1, comment: 1 }, { unique: true, sparse: true });
+
+export const Like = mongoose.model('Like', likeSchema);
